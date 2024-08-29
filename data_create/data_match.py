dea87aa770aeb9c2cf2db9eb81fe2ff40eeab730
Wapiti08 <tzrzhuoran@gmail.com>
2024-08-29 12:01:54 +0100

    testing bkc match

--- data_create/data_match.py
+++ data_create/data_match.py
@@ -59,6 +59,13 @@ def pkg_label_match(pkg_data_file, pkg_mal_file, save_path):
     return pkg_df
 
 
+def pkg_bkc_match():
+    ''' match labels from BKC dataset
+    
+    '''
+    pass
+
+
 def feature_ext(pkg_data):
     ''' extract features from package-analysis dataset
     
@@ -66,6 +73,32 @@ def feature_ext(pkg_data):
     pass
 
     
+
+def bkc_json_to_csv(mal_pkg_folder, save_path):
+    ''' collect all json files and convert them to csv
+    
+    '''
+    mal_dict_list = []
+    for json_file in mal_pkg_folder.rglob("**/*.json"):
+        # bypass some directories names with .json
+        if json_file.is_file():
+            json_info = load_json_file(json_file)
+
+        affect_dict = json_info["affected"][0]
+        try:
+            version = affect_dict["versions"][0]
+        except:
+            # there is no version information for some ecosystem (npm)
+            version = ""
+
+        mal_dict_list.append(
+            {"ecosystem": affect_dict["package"]["ecosystem"],
+             "name": affect_dict["package"]["name"],
+             "version": version}
+        )
+
+    df = pd.DataFrame(mal_dict_list)
+    df.to_csv(save_path.joinpath("pkg_mal.csv"), index=False)
 
 
 def json_to_csv(mal_pkg_folder, save_path):
