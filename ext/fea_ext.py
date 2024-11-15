3f672356366a48d4222f88780e4d7c42d428eb1a
Wapiti08 <tzrzhuoran@gmail.com>
2024-11-15 17:23:16 +0000

    start to parse json file to features

---
+++ ext/fea_ext.py
@@ -0,0 +1,23 @@
+'''
+ # @ Create Time: 2024-10-23 11:14:09
+ # @ Modified time: 2024-11-15 15:40:35
+ # @ Description:
+ '''
+from pathlib import Path
+
+
+def json_parse():
+
+
+
+def label_data():
+
+
+
+if __name__ == "__main__":
+    example_json = Path.cwd().parent.joinpath("data","package-analysis-mal","results","npm-azure-ai-anomaly-detector-.json")
+    
+
+
+
+
