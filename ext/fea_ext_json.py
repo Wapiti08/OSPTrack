b882712f414fe411e2216d2ea77e40e481704259
Wapiti08 <tzrzhuoran@gmail.com>
2024-11-16 12:31:55 +0000

    update names and version in data_metrics

---
+++ ext/fea_ext_json.py
@@ -0,0 +1,32 @@
+'''
+ # @ Create Time: 2024-10-23 11:14:09
+ # @ Modified time: 2024-11-15 15:40:35
+ # @ Description: parse features from simulated reports in json format
+ 
+ 
+ '''
+from pathlib import Path
+
+class JsonParser:
+    def __init__():
+                
+
+    def json_parse():
+    
+
+    def label_create():
+
+    
+    def sub_label_create():
+    
+
+if __name__ == "__main__":
+    
+    data_path = Path.cwd().parent.joinpath("data","package-analysis-mal","results")
+
+
+    
+
+
+
+
